d3a98def822753aedb5c4ccb5f9a8b305b08615c
Harold Parra <gc.harold.parra@gmail.com>
2020-12-27 19:17:01 +0000

    updated weekday targeting assignment

--- lib/time_targeting.rb
+++ lib/time_targeting.rb
@@ -19,6 +19,25 @@ def sort_hash(hash)
   hash.sort_by {|key,value| value}.reverse!.to_h
 end
 
+def weekday_decipher(weekday_code)
+  case weekday_code
+  when 0
+    "Sunday"
+  when 1
+    "Monday"
+  when 2
+    "Tuesday"
+  when 3
+    "Wednesday"
+  when 4
+    "Thursday"
+  when 5
+    "Friday"
+  when 6
+    "Saturday"
+  end
+end
+
 puts "EventManager Initialized!"
 
 #READING CSV FILE USING CSV FOR LATER USE IN THE PROGRAM
@@ -26,9 +45,6 @@ puts "EventManager Initialized!"
   #- HEADERS
   #- HEADERS AS SYMBOLS
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
-
-#USING ERB FILE INSTEAD OF COMMON TEXT FILE TO AVOID SOME PROBLEMS
-template_letter = File.read "form_letter.erb"
 
 hours_frequency = Hash.new
 weekday_frequency = Hash.new
@@ -38,7 +54,7 @@ contents.each do |row|
   hour_registered = DateTime.strptime(row[:regdate], '%m/%d/%y %H:%M').hour
 
   #USNG DATE TO FIND THE DAY OF THE WEEK OF REGISTRATION OF EACH PERSON
-  weekday_registered = DateTime.strptime(row[:regdate], '%m/%d/%y %H:%M').wday
+  weekday_registered = weekday_decipher(DateTime.strptime(row[:regdate], '%m/%d/%y %H:%M').wday)
 
   #TABULATING HOUR FREQUENCY INTO A HASH
   hours_frequency = tabulate_frequency(hours_frequency, hour_registered)
